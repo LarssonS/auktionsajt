18484693ea2060ae4a365cb82253624d2df4976b
Sofiaskogelin <31960126+Sofiaskogelin@users.noreply.github.com>
2018-03-14 10:32:47 +0100

    lagt till post

---
+++ scripts/api-get-data.js
@@ -0,0 +1,63 @@
+GetData();
+async function GetData()
+{
+  let response = await FetchData ("http://nackowskis.azurewebsites.net/api/auktion/600/")
+  console.log(response);
+
+//let parseResponse = JSON.stringify(response);
+
+for (var i = 0; i < response.length; i++) {
+  response[i]
+
+  let auktionUl=document.createElement("ul");
+    let auktionPTag = document.createElement("li");
+    let auktionText = document.createTextNode(response[i].AuktionID + response[i].Beskrivning +response[i].SlutDatum + response [i].StartDatum + response[i].Titel + response[i].Utropspris);
+    auktionPTag.appendChild(auktionText);
+    auktionUl.appendChild(auktionPTag)
+    document.body.appendChild(auktionUl);
+}
+
+/*  let auktionPTag = document.createElement("p");
+  let auktionText = document.createTextNode(response[i].AuktionID + response[i].Beskrivning +response[i].SlutDatum + response [i].StartDatum + response[i].Titel + response[i].Utropspris);
+  auktionPTag.appendChild(auktionText);
+  document.body.appendChild(auktionPTag);
+}
+  /*for (var auktion of response)
+  {
+    let auktionPTag = document.createElement("p");
+    let auktionText = document.createTextNode(auktion);
+    auktionPTag.appendChild(auktionText);
+    document.body.appendChild(auktionPTag);
+  }*/
+}
+
+async function FetchData(url)
+{
+  let promise = await fetch (url);
+  let data = await promise.json();
+  return data;
+}
+
+function postData(){
+fetch("http://nackowskis.azurewebsites.net/api/auktion/",{
+ method: 'POST',
+ body: JSON.stringify({
+      "AuktionID": 3,
+      "Titel": "Glass",
+       "Beskrivning": "blablabla",
+       "StartDatum": "2018-01-10T00:00:00",
+       "SlutDatum": "2018-01-10T00:00:00",
+       "Gruppkod": 600,
+       "Utropspris": 300,
+     }),
+
+ headers: {
+ 'Accept': 'application/json, text/plain, */*',
+ 'Content-Type': 'application/json'
+ }
+ }).then(function (data) {
+ console.log('Request success: ', 'posten skapad');
+})
+}
+
+//postData();
---
+++ scripts/api-post-data.js
@@ -0,0 +1,10 @@
+var auktion1()
+{
+AuktionID: 6,
+Beskrivning: "Tidig Lisa Lind målning. Olja på duk, signerad och daterad -71"
+Gruppkod: 600,
+SlutDatum: "2018-03-31T00:00:00"
+StartDatum: "2018-03-10T00:00:00"
+Titel: "Lisa Lind oljemålning över Stockholm"
+Utropspris: 1500,
+}
